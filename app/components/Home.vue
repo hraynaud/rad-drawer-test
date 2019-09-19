58dd91f0b0f2a2d15dbcead0d633135a912b39f7
Herby Raynaud <herby@yieldmo.com>
2019-09-18 23:03:32 -0400

    Show projects list

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,57 +1,79 @@
 <template>
-    <Page class="page">
-        <ActionBar class="action-bar">
-            <!-- 
+  <Page class="page">
+    <ActionBar class="action-bar">
+      <!-- 
             Use the NavigationButton as a side-drawer button in Android
             because ActionItems are shown on the right side of the ActionBar
-            -->
-            <NavigationButton ios:visibility="collapsed" icon="res://menu" @tap="onDrawerButtonTap"></NavigationButton>
-            <!-- 
+      -->
+      <NavigationButton ios:visibility="collapsed" icon="res://menu" @tap="onDrawerButtonTap"></NavigationButton>
+      <!-- 
             Use the ActionItem for IOS with position set to left. Using the
             NavigationButton as a side-drawer button in iOS is not possible,
             because its function is to always navigate back in the application.
-            -->
-            <ActionItem icon="res://navigation/menu" 
-                android:visibility="collapsed" 
-                @tap="onDrawerButtonTap"
-                ios.position="left">
-            </ActionItem>
-            <Label class="action-bar-title" text="Home"></Label>
-        </ActionBar>
+      -->
+      <ActionItem
+        icon="res://navigation/menu"
+        android:visibility="collapsed"
+        @tap="onDrawerButtonTap"
+        ios.position="left"
+      ></ActionItem>
+      <Label class="action-bar-title" text="Home"></Label>
+    </ActionBar>
 
-        <GridLayout class="page-content">
-            <Label class="page-icon fa" text.decode="&#xf015;"></Label>
-            <Label class="page-placeholder" :text="message"></Label>
-        </GridLayout>
-
-    </Page>
+    <GridLayout class="page-content">
+       <ListView for="p in projects" class="list-group">
+          <v-template>
+            <StackLayout class="list-group-item">
+              <Label :text="p.name" />
+            </StackLayout>
+          </v-template>
+        </ListView>
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-    import * as utils from "~/shared/utils";
-    import SelectedPageService from "../shared/selected-page-service";
+import * as utils from "~/shared/utils";
+import { apiService } from "../services/api.service";
+import SelectedPageService from "../shared/selected-page-service";
 
-    export default {
-        mounted() {
-            SelectedPageService.getInstance().updateSelectedPage("Home");
-        },
-        computed: {
-            message() {
-                return "<!-- Page content goes here -->";
-            }
-        },
-        methods: {
-            onDrawerButtonTap() {
-                utils.showDrawer();
-            }
-        }
+export default {
+  data() {
+    return {
+      projects: []
     };
+  },
+  mounted() {
+    SelectedPageService.getInstance().updateSelectedPage("Home");
+    console.log("!! local IP", LOCAL_IP);
+    apiService.get("api/v1/projects").then(
+      data => {
+        this.projects = data.sort(function(a, b) {
+          return a.name > b.name ? 1 : -1;
+        });
+      },
+      error => {
+        console.error("!!!!", error);
+      }
+    );
+  },
+  computed: {
+    message() {
+      return "<!-- Page content goes here -->";
+    }
+  },
+  methods: {
+    onDrawerButtonTap() {
+      utils.showDrawer();
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    // Start custom common variables
-    @import '../app-variables';
-    // End custom common variables
+// Start custom common variables
+@import "../app-variables";
+// End custom common variables
 
-    // Custom styles
+// Custom styles
 </style>
