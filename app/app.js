43da777cdde93e288839e1805860ddb0a28a0076
Herby Raynaud <herbyraynaud@ip-192-168-1-101.ec2.internal>
2020-03-28 08:11:50 -0400

    Install iqkeyboard manager

--- app/app.js
+++ app/app.js
@@ -4,19 +4,20 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import DrawerContent from "./components/DrawerContent";
 import RadSideDrawer from "nativescript-ui-sidedrawer/vue";
-import { authService } from './services/auth.service'
+import { authService } from "./services/auth.service";
+Vue.registerElement(
+    "PreviousNextView",
+    () => require("nativescript-iqkeyboardmanager").PreviousNextView
+);
 Vue.use(RadSideDrawer);
 
-Vue.config.silent = (TNS_ENV === 'production');
+Vue.config.silent = TNS_ENV === "production";
 
 new Vue({
-  render(h) {
-    return h(
-      App,
-      [
-        h(DrawerContent, { slot: 'drawerContent' }),
-        h((authService.isLoggedIn() ? Home : Login), { slot: 'mainContent' })
-      ]
-    )
-  }
+    render(h) {
+        return h(App, [
+            h(DrawerContent, { slot: "drawerContent" }),
+            h(authService.isLoggedIn() ? Home : Login, { slot: "mainContent" })
+        ]);
+    }
 }).$start();
